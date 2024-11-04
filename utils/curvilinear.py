206474a507dcfefc295799442dbccf753e405498
getrichthroughcode <abdoulayediallo338@gmail.com>
2024-11-04 08:54:31 +0100

    quick fix

---
+++ utils/curvilinear.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def simulate_curvilinear_motion(T, length, x_0):
+    
+    states = np.zeros((6, length))
+    states[:, 0] = x_0
+
+    
+    Q = np.array([[T**3 / 3, T**2 / 2, 0, 0],  
+                  [T**2 / 2, T, 0, 0],
+                  [0, 0, T**3 / 3, T**2 / 2],
+                  [0, 0, T**2 / 2, T]])
+
+    for k in range(1, length):
+        
+        x, v_x, y, v_y, a_t, a_n = states[:, k-1]
+
+        
+        theta_k = np.arctan2(v_y, v_x)
+        
+        # Définir la matrice de transition d'état F
+        F = np.array([
+            [1, T, 0, 0, 0, 0],
+            [0, 1, 0, 0, T * np.cos(theta_k), -T * np.sin(theta_k)],
+            [0, 0, 1, T, 0, 0],
+            [0, 0, 0, 1, T * np.sin(theta_k), T * np.cos(theta_k)],
+            [0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 1]
+        ])
+
+        # Bruit de processus
+        w_k = np.random.multivariate_normal(mean=[0, 0, 0, 0, 0, 0], cov=Q)
+
+        # Mettre à jour l'état
+        states[:, k] = F @ states[:, k-1] + w_k
+
+    return states
