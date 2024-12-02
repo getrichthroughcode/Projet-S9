99d88236969d51ae3b05c01f885a028312a4d9e3
Nartbeonline <losilla.romain.ll@gmail.com>
2024-12-02 09:14:55 +0100

    Archi CNN

--- CNN_proj.py
+++ CNN_proj.py
@@ -11,15 +11,15 @@ class CNN(nn.Module):
     def __init__(self, num_classes):
         super(CNN, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv1d(1, 16, kernel_size=5, stride=2, padding=2),#50
+            nn.Conv1d(1, 16, kernel_size=5, stride=1, padding=2),#100
             nn.ReLU())
         self.layer2 = nn.Sequential(
-            nn.Conv1d(16, 32, kernel_size=5, stride=2, padding=2),#25
+            nn.Conv1d(16, 32, kernel_size=5, stride=1, padding=2),#100
             nn.ReLU())
         self.layer3 = nn.Sequential(
-            nn.Conv2d(32, 32, kernel_size=5, stride=2, padding=2),#12
+            nn.Conv1d(32, 32, kernel_size=5, stride=1, padding=2),#100
             nn.ReLU())
-        self.fc = nn.Linear(12 * 32, num_classes)
+        self.fc = nn.Linear(100 * 32, num_classes)
 
     def forward(self, x):
         out = self.layer1(x)
