a3efa517e3fb0a59f119f0570916a3d7b6579a6b
Simon <67315072+Chokidrill@users.noreply.github.com>
2024-12-02 09:23:32 +0100

    Add files via upload

--- read_csv.py
+++ read_csv.py
@@ -20,8 +20,20 @@ def read_csv(path_folder, path_file, list_name):
                 out[list_name.index(row[0])] = row[1:]
     return out
             
-            
+def operation_csv_folder(path_folder, name_file, nb_file, func, list_name):
+    for i in range(nb_file):
+        path_file = path_folder + '/' + path_file + "_{i}.csv"
+        return
+        
         
 if __name__ == "__main__":
-    print(read_csv("2D_MUA","0_MUA",['X','Title','aX']))
+    print(read_csv("Singer","0_Singer",['aX','Title']))
+    ans = read_csv("2D_MUA","0_MUA",['aX','Title'])
+    mean = 0
+    for i in range(len(ans[0])):
+        print(float(ans[0][i]))
+        float(ans[0][i])
+    print(mean/len(ans[0]))
+    
+    
     
